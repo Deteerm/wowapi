cb32fe855036dfba57a0ca18de8ca0ea4958b68e
dominik.tarkiewicz <dominik.tarkiewicz@notus.pl>
2022-02-11 08:59:12 +0100

    set up simplest server

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+require('dotenv').config()
 const express = require("express");
 
 const port = process.env.PORT || 1337;
 
-// test commit
+const app = express();
+
+app.get('/', get)
+
+app.listen(port, () => console.log(`Server listening on port: ${port}`))
+
+function get(req, res) {
+    res.json("Hello world!")
+}
