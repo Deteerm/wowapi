49c0c16fc93ec3b52c5092e20ed4212c25cbd7b3
Dominik Tarkiewicz <dominik.tarkiewicz@gmail.com>
2022-02-11 21:53:54 +0100

    auto catching

--- api.js
+++ api.js
@@ -1,32 +1,27 @@
-const Champions = require('./champions')
+const Champions = require("./champions");
+const autoCatch = require("./lib/auto-catch");
 
-module.exports = {
-    listChampions,
-    getChampion
-}
+module.exports = autoCatch({
+  getChampion,
+  listChampions,
+});
 
 async function listChampions(req, res, next) {
-    const  {offset = 0, limit = 10, faction} = req.query
-    try {
-        res.json(await Champions.list({
-            offset: Number(offset),
-            limit: Number(limit),
-            faction: faction
-        }))
-    } catch(err) {
-        next(err)
-    }
+  const { offset = 0, limit = 10, faction } = req.query;
+  res.json(
+    await Champions.list({
+      offset: Number(offset),
+      limit: Number(limit),
+      faction: faction,
+    })
+  );
 }
 
 async function getChampion(req, res, next) {
-    const {name} = req.params
+  const { name } = req.params;
 
-    try {
-        const champion = await Champions.get(name)
-        if (!champion) next()
+  const champion = await Champions.get(name);
+  if (!champion) next();
 
-        res.json(champion)
-    } catch(err) {
-        next(err)
-    }
+  res.json(champion);
 }
